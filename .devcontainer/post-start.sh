23f5678e4252c898b06365bf20c90111e319da47
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-15 14:48:00 +1000

    update

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DEBUG_VERSION=1
+
+echo "post-start start" >> ~/status
+
+# this runs in background each time the container starts
+
+# update the base docker images
+#docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
+#docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+#docker pull mcr.microsoft.com/dotnet/sdk:5.0
+
+echo "post-start complete" >> ~/status
+
+# Run some logic
+# If logic is OK: Send bizevent: OK
+# If logic !OK: Send bizevent: error
+cd ..
+output=$(ls -al ~/mclass)
+if [[ $output =~ "test.txt" ]];
+then
+  curl -X POST https://webhook.site/1e9185e3-1225-439b-8381-044fd1f417cc \
+    --header "Content-Type: application/json" \
+    -d "{\"result\": \"$DEBUG_VERSION\": \"YUP\"}"
+else
+  curl -X POST https://webhook.site/1e9185e3-1225-439b-8381-044fd1f417cc \
+    --header "Content-Type: application/json" \
+    -d "{\"result\": \"$DEBUG_VERSION\": \"NOPE\"}"
+fi
+
+# Finally, destroy the codespace
+#gh codespace delete --codespace $CODESPACE_NAME
