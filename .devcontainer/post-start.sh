2941547bb8144ccb5a190ac2ecf902af28710dbc
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-16 00:47:10 +0000

    update

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "post-start start" >> ~/status
+echo "[post-start] start" >> ~/status
 
 # Start the cluster
 minikube start
 
+echo "[post-start] minikube available" >> ~/status
+
 # install argocd
 kubectl create namespace argocd
+echo "[post-start] argocd namespace created" >> ~/status
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=Available=True deploy -n argocd --all --timeout=90s
+echo "[post-start] argocd deployed" >> ~/status
 
 ##########################
 # 1. Install test harness dependencies
 # 2. Run test harness
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
-python testharness.py
+python ../testharness.py
 
 echo "post-start complete" >> ~/status
