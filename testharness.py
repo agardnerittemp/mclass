f1cbafb55eda06072e70fd666e126a748164b5eb
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-16 01:29:07 +0000

    a

--- testharness.py
+++ testharness.py
@@ -1,33 +1,40 @@
 import subprocess
 import requests
 
-def sendNotification(success, error_string=""):
+def sendNotification(success, msg_string="", destroy_codespace=False):
 
     url = "https://webhook.site/1e9185e3-1225-439b-8381-044fd1f417cc"
 
     payload = {
         "success": success,
-        "error": error_string
+        "msg": msg_string
     }
 
     response = requests.post(url=url, json=payload)
 
-    print(f"Response: {response.status_code}")
+    # TODO
+    # If user wishes to immediately 
+    #if destroy_codespace:
+    # destroyCodespace()
 
+# Do not call this directly
+# Instead, use `sendNotification(..., destroy_codespace=True)`
 #def destroyCodespace():
     # TODO
+
 
 #################################
 # Main test harness
 #################################
 try:
+    # Test 1: Does argocd namespace exist
     output = subprocess.run(["kubectl", "get", "namespaces"], capture_output=True, text=True) # works
 
     if "argocd" not in output.stdout:
-        sendNotification(success=False, error_string="argocd namespace missing")
-        # TODO
-        # destroyCodespace()
+        sendNotification(success=False, msg_string="argocd namespace missing", destroy_codespace=True)
+        
 except Exception as e:
-    sendNotification(success=False, error_string=str(e))
-    # TODO
-    # destroyCodespace()
+    sendNotification(success=False, msg_string=str(e), destroy_codespace=True)
+
+# test harness successful. Send congrats message
+sendNotification(success=True, msg_string="test harness executed successfully")
