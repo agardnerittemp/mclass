3c2e98ae4b8cadd41a65f9180a06aa0f9fbf5637
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-16 10:12:28 +1000

    update

--- testharness.py
+++ testharness.py
@@ -1,8 +1,18 @@
 import subprocess
 import requests
 
-def sendNotification(url, success):
-    return "TODO"
+def sendNotification(success):
+
+    url = "https://webhook.site/1e9185e3-1225-439b-8381-044fd1f417cc"
+
+    payload = {
+        "foo": "bar",
+        "success": success
+    }
+
+    response = requests.post(url=url, json=payload)
+
+    print(f"Response: {response.status_code}")
 
 try:
     #output = subprocess.run(["ls", "-al"], capture_output=True, text=True) # works
