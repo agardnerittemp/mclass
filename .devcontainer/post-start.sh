5acc1239eb2418d38bcde504250062b08f4ca184
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-17 13:14:49 +1000

    update

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,11 +7,18 @@ kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 
 echo "[post-start] cluster available" >> ~/status
 
+# Download argocd CLI
+# Download the argocd CLI and authenticate
+wget -O argocd https://github.com/argoproj/argo-cd/releases/download/v2.9.3/argocd-linux-amd64
+chmod +x argocd
+sudo mv argocd /usr/bin
+
 # install argocd
 kubectl create namespace argocd
 echo "[post-start] argocd namespace created" >> ~/status
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=Available=True deploy -n argocd --all --timeout=300s
+
 echo "[post-start] argocd deployed" >> ~/status
 
 
@@ -19,10 +26,37 @@ kubectl apply -n argocd -f gitops/manifests/platform/argoconfig/argocd-no-tls.ym
 kubectl apply -n argocd -f gitops/manifests/platform/argoconfig/argocd-nodeport.yml
 kubectl -n argocd rollout restart deploy/argocd-server
 kubectl -n argocd rollout status deploy/argocd-server --timeout=300s
+
+# Set the default context to the argocd namespace so 'argocd' CLI works
+kubectl config set-context --current --namespace=argocd
+# Now authenticate
+argocd login argo --core
+
+# Set the default context to the argocd namespace so 'argocd' CLI works
+ARGOCD_TOKEN=$(argocd account generate-token --account alice)
+
+# Reset kubectl context
+kubectl config set-context --current --namespace=default
+
 echo "[post-start] argocd configured and restarted" >> ~/status
 
 kubectl create namespace opentelemetry
-kubectl -n opentelemetry create secret generic dt-details --from-literal=DT_URL=$DT_TENANT_LIVE --from-literal=DT_OTEL_ALL_INGEST_TOKEN=$DT_ALL_INGEST_TOKEN
+kubectl -n opentelemetry create secret generic dt-details \
+  --from-literal=DT_URL=$DT_TENANT_LIVE \
+  --from-literal=DT_OTEL_ALL_INGEST_TOKEN=$DT_ALL_INGEST_TOKEN
+
+
+kubectl create namespace backstage
+kubectl -n backstage create secret generic backstage-secrets \
+  --from-literal=BASE_DOMAIN=$CODESPACE_NAME \
+  --from-literal=BACKSTAGE_PORT_NUMBER=7007 \
+  --from-literal=ARGOCD_PORT_NUMBER=30100 \
+  --from-literal=ARGOCD_TOKEN=$ARGOCD_TOKEN \
+  --from-literal=GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN=$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN \
+  --from-literal=GITHUB_TOKEN=$GITHUB_TOKEN \
+  --from-literal=GITHUB_USER=$GITHUB_USER \
+  --from-literal=DT_TENANT_LIVE=$DT_TENANT_LIVE \
+  --from-literal=DT_EVENT_INGEST_TOKEN=$DT_ALL_INGEST_TOKEN
 
 # Install platform
 kubectl apply -f gitops/platform.yml
