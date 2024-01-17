f535be38c42b5266ffae6b5f1e97ff3d0c5fd0f7
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-17 15:28:15 +1000

    update

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -55,7 +55,13 @@ kubectl -n backstage create secret generic backstage-secrets \
   --from-literal=GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN=$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN \
   --from-literal=GITHUB_TOKEN=$GITHUB_TOKEN \
   --from-literal=GITHUB_USER=$GITHUB_USER \
+  --from-literal=DT_TENANT_NAME=$DT_TENANT_NAME \
   --from-literal=DT_TENANT_LIVE=$DT_TENANT_LIVE \
+  --from-literal=DT_TENANT_APPS=$DT_TENANT_APPS \
+  --from-literal=DT_SSO_TOKEN_URL=$DT_SSO_TOKEN_URL \
+  --from-literal=DT_OAUTH_CLIENT_ID=$DT_OAUTH_CLIENT_ID \
+  --from-literal=DT_OAUTH_CLIENT_SECRET=$DT_OAUTH_CLIENT_SECRET \
+  --from-literal=DT_OAUTH_ACCOUNT_URN=$DT_OAUTH_ACCOUNT_URN \
   --from-literal=DT_EVENT_INGEST_TOKEN=$DT_ALL_INGEST_TOKEN
 
 kubectl create namespace dynatrace
