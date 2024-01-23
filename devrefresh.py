f79c11a029be2a876d6087ebd68634c5a60af404
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-23 15:15:46 +1000

    update

--- devrefresh.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-
-run_command(["rm", "-rf", f"/workspaces/{GITHUB_REPO_NAME}"])
-
-input("Press a key when the fork is synced to upstream. Waiting....")
-
-run_command(["git", "clone", GITHUB_DOT_COM_REPO, f"/workspaces/{GITHUB_REPO_NAME}"])
-
-print(f"All done. Now run: `python3 /workspaces/{GITHUB_REPO_NAME}/cluster_installer.py`")
