72fd86ea5804449dbf6104a0d9abfe52dd5de526
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-23 15:09:58 +1000

    update

--- devrefresh.py
+++ devrefresh.py
@@ -1,10 +1,9 @@
 from utils import *
 
-run_command(["rm", "-rf", "/workspaces/mclass"])
+run_command(["rm", "-rf", f"/workspaces/{GITHUB_REPO_NAME}"])
 
 input("Press a key when the fork is synced to upstream. Waiting....")
 
-run_command(["cd", "/workspaces"])
-run_command("git", "clone", GITHUB_DOT_COM_REPO)
+run_command("git", "clone", GITHUB_DOT_COM_REPO, f"/workspaces/{GITHUB_REPO_NAME}")
 
-print("All done. Now run: `python3 /workspaces/mclass/cluster_installer.py`")
+print(f"All done. Now run: `python3 /workspaces/{GITHUB_REPO_NAME}/cluster_installer.py`")
